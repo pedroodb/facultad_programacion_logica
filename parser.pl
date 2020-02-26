346007fd66527e73ff74e01b83fedcb0d91c09f9
pedroodb <dalbianco.pedro@gmail.com>
2020-02-25 21:49:54 -0300

    Descripcion lenguaje

--- parser.pl
+++ parser.pl
@@ -5,3 +5,15 @@
 
 parse(X) :- phrase(block, X).
 
+/*
+
+Parser pensado para un lenguaje imperativo simple, en el que se dispone de: 
+
+- asignaciones: variable = expresion
+- condicionales: if (expresion booleana) bloque 'else' bloque
+- repeticiones: repeat (expresion numerica) bloque
+- loops condicionales: while (expresion booleana) bloque
+
+Los bloques deben escribirse entre llaves ({}), y las lineas deben finalizar con ';'.
+
+*/
